9f3016f707856e4d3e75981be5c55fa6908f7c45
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-02-07 00:15:27 +0530

    basic responder fo rthe list

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -2,16 +2,19 @@ import { View, Animated, Text, PanResponder } from 'react-native'
 import { useRef } from 'react';
 const Deck = ({ data, renderCard }) => {
 
+  const pan = useRef(new Animated.ValueXY()).current;
 
   const panResponder = useRef(PanResponder.create({
     // Decide whether this responder should handle the event.
     // Triggerred when user first clicks down.
     onStartShouldSetPanResponder: (evt, gestureState) => true,
     // Called many many times as the use is dragging the element around the screen
-    onPanResponderMove: (evt, gestureState) => { 
-      console.log(evt);
-      console.log(gestureState);
-    },
+    // Not sure about this, but only one gestureState object exists.
+    // As soon as we exit onPanResponderMove the gesture object is reset.
+    // Only one exists in physical memory
+    // https://reactnative.dev/docs/panresponder
+    // Try changing with number and see what happens
+    onPanResponderMove: Animated.event([null, {dx: pan.x, dy: pan.y}], {useNativeDriver: false}),
     // Called when user presses down, and then releases.
     // Finalised callback.
     onPanResponderRelease: () => {},
@@ -22,7 +25,13 @@ const Deck = ({ data, renderCard }) => {
   }
 
   return (
-    <View {...panResponder.panHandlers}>{renderCards()}</View>
+    <Animated.View 
+    style = {{  transform : [
+    { translateX: pan.x}, 
+     {translateY: pan.y}
+  ]}}
+    {...panResponder.panHandlers}>{renderCards()}
+    </Animated.View>
   )
 }
 
