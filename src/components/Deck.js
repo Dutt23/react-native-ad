51658c057d215c7bf480fd2d9ce8a0923019b3a5
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-02-08 02:02:55 +0530

    Linter

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -5,7 +5,7 @@ const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = .50 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
-const Deck = ({ data, renderCard, onSwipeRight = (item) =>{}, onSwipeLeft = (item) => {}, renderNoMoreCards }) => {
+const Deck = ({ data, renderCard, onSwipeRight = (item) => { }, onSwipeLeft = (item) => { }, renderNoMoreCards }) => {
 
 
   const [counter, setCounter] = useState(0);
@@ -21,89 +21,94 @@ const Deck = ({ data, renderCard, onSwipeRight = (item) =>{}, onSwipeLeft = (ite
     // Only one exists in physical memory
     // https://reactnative.dev/docs/panresponder
     // Try changing with number and see what happens
-    onPanResponderMove: Animated.event([null, {dx: pan.x, dy: pan.y}], {useNativeDriver: false}),
+    onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }], { useNativeDriver: false }),
     // Called when user presses down, and then releases.
     // Finalised callback.
     onPanResponderRelease: (evt, gestureState) => {
       // If you need to keep the current position as it is.
       //  pan.extractOffset() 
       const { dx } = gestureState;
-      if(dx > SWIPE_THRESHOLD){
+      if (dx > SWIPE_THRESHOLD) {
         forceSwipe('right')
       } else if (dx < -SWIPE_THRESHOLD) {
         forceSwipe('left')
       } else {
         resetPosition()
       }
-      },
+    },
   })).current;
 
 
   const getCardStyle = () => {
     const rotate = pan.x.interpolate({
-      inputRange:[-SCREEN_WIDTH * 1.5 , 0, SCREEN_WIDTH * 1.5],
-      outputRange:['-120deg', '0deg', '120deg']
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg']
     });
 
-    return { transform : [
-      { translateX: pan.x }, 
-      { translateY: pan.y },
-      { rotate },
-    ]}
+    return {
+      transform: [
+        { translateX: pan.x },
+        { translateY: pan.y },
+        { rotate },
+      ]
+    }
   }
 
-  const forceSwipe = (action) =>{
+  const forceSwipe = (action) => {
     Animated.timing(pan, {
-      toValue: { x : action === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH,  y : 0},
+      toValue: { x: action === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH, y: 0 },
       duration: SWIPE_OUT_DURATION,
       useNativeDriver: false,
     }).start(() => onSwipeComplete(action));
   }
 
   useEffect(() => {
-    pan.setValue({ x:0, y:0 })
+    pan.setValue({ x: 0, y: 0 })
 
-    return () => {}
+    return () => { }
   }, [counter])
 
-  const onSwipeComplete = (direction) =>{
+  const onSwipeComplete = (direction) => {
     setCounter((prev) => {
       const item = data[prev];
       direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item)
-      const newCounter = prev +1;
+      const newCounter = prev + 1;
       return newCounter;
     })
   }
 
-  const resetPosition = () =>{
+  const resetPosition = () => {
     Animated.spring(pan, {
-      toValue: { x :0, y : 0},
+      toValue: { x: 0, y: 0 },
       useNativeDriver: false,
     }).start()
   }
 
   const renderCards = () => {
 
-    if(counter >= data.length){
+    if (counter >= data.length) {
       return renderNoMoreCards();
     }
 
     return data.map((item, index) => {
-      if(index < counter) { return null; }
+      if (index < counter) { return null; }
 
-      return index === counter ? 
-      (<Animated.View 
-        key={item.id}
-        style = {[getCardStyle(), styles.cardStyle]}
-        {...panResponder.panHandlers}>
+      return index === counter ?
+        (<Animated.View
+          key={item.id}
+          style={[getCardStyle(), styles.cardStyle]}
+          {...panResponder.panHandlers}>
           {renderCard(item)}
-        </Animated.View>) 
-        :  
+        </Animated.View>)
+        :
         (
-        <Animated.View key ={item.id} style ={styles.cardStyle}>
-          {renderCard(item)}
-        </Animated.View>
-      )
+          // When the card reaches the top deck, it needs to be rendered with animated view component
+          // So it gets promoted from View to animated view, so it gets re-rendered.
+          // Hence the device tries to fetch the image again, and a small flicker of the image happens
+          <Animated.View key={item.id} style={styles.cardStyle}>
+            {renderCard(item)}
+          </Animated.View>
+        )
     }).reverse();
     // Absolute property renders one by one. On top of each other, from last element.
     // Reversing the list to show the last one first.
